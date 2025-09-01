e608212b92486067000bad7dde9fdf6a12f64734
Paul Taylor <81833345+PuaNow@users.noreply.github.com>
2025-09-01 16:45:17 -0700

    update 2025

--- chicken_door/chicken_door.ino
+++ chicken_door/chicken_door.ino
@@ -1,41 +1,32 @@
-// Adafruit IO Servo Example
-// Tutorial Link: https://learn.adafruit.com/adafruit-io-basics-servo
-//
-// Adafruit invests time and resources providing this open source code.
-// Please support Adafruit and open source hardware by purchasing
-// products from Adafruit!
-//
-// Written by Todd Treece for Adafruit Industries
-// Copyright (c) 2016-2017 Adafruit Industries
-// Licensed under the MIT license.
-//
-// All text above must be included in any redistribution.
-
-/************************** Configuration ***********************************/
-
-// edit the config.h tab and enter your Adafruit IO credentials
-// and any additional configuration needed for WiFi, cellular,
-// or ethernet clients.
 #include "config.h"
-
+#include <AdafruitIO.h>
+#include <AdafruitIO_WiFi.h>
 #include <Wire.h>
-
 #include <Adafruit_MotorShield.h>
+
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
 AdafruitIO_Feed *digital = io.feed("digital");
 
+unsigned long motorStartTime = 0;
+bool motorRunning = false;
+int motorDirection = RELEASE;
+
 void setup() {
- 
   Serial.begin(115200);
-  while(! Serial);
+  unsigned long start = millis();
+  while (!Serial && millis() - start < 1000); // Wait up to 1s for Serial
+  Serial.println("Starting...");
+
   AFMS.begin();
   myMotor->setSpeed(200);
+  Serial.println("Motor Shield initialized");
+
   Serial.print("Connecting to Adafruit IO");
   io.connect();
   digital->onMessage(handleMessage);
 
-  while(io.status() < AIO_CONNECTED) {
+  while (io.status() < AIO_CONNECTED) {
     Serial.print(".");
     delay(500);
   }
@@ -43,39 +34,44 @@ void setup() {
   Serial.println();
   Serial.println(io.statusText());
   digital->get();
-
 }
+
 void loop() {
- uint8_t i;
   io.run();
+  if (motorRunning && millis() - motorStartTime >= (motorDirection == FORWARD ? 65000UL : 60000UL)) {
+    myMotor->run(RELEASE);
+    motorRunning = false;
+    Serial.println("Motor stopped");
+  }
 }
 
 void handleMessage(AdafruitIO_Data *data) {
- int reading = data->toInt();
+  int reading = data->toInt();
   Serial.print("received <- ");
+  Serial.println(reading);
 
-  if (reading == 1){
+  if (reading == 1 && !motorRunning) {
     Serial.println("Opening");
-   myMotor->run(FORWARD);
-  delay(65000);
-  myMotor->run(RELEASE);}
-  else
-    Serial.println("Already Open");
-
-  
-  if (reading == 2){
+    myMotor->setSpeed(200);
+    myMotor->run(FORWARD);
+    motorStartTime = millis();
+    motorRunning = true;
+    motorDirection = FORWARD;
+  } else if (reading == 2 && !motorRunning) {
     Serial.println("Closing");
-     myMotor->run(BACKWARD);
-  delay(65000);
-  myMotor->run(RELEASE);}
-   else
-    Serial.println("Already closed");
-         
-     if (reading == 3){
+    myMotor->setSpeed(200);
+    myMotor->run(BACKWARD);
+    motorStartTime = millis();
+    motorRunning = true;
+    motorDirection = BACKWARD;
+  } else if (reading == 3 && !motorRunning) {
     Serial.println("Resetting");
-     myMotor->run(BACKWARD);
-  delay(60000);
-  myMotor->run(RELEASE);}
-   else
-    Serial.println("Already Open");
+    myMotor->setSpeed(200);
+    myMotor->run(BACKWARD);
+    motorStartTime = millis();
+    motorRunning = true;
+    motorDirection = BACKWARD;
+  } else {
+    Serial.println(motorRunning ? "Motor busy" : "Invalid command");
+  }
 }
